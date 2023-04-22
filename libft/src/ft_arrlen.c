a20382c2c48ec0e5119cb6a2cab68a4a858d4c8a
Sarah Mclacken <smclacke@f0r2s18.codam.nl>
2023-04-22 19:31:36 +0200

    testing

--- libft/src/ft_arrlen.c
+++ libft/src/ft_arrlen.c
@@ -6,11 +6,13 @@
 /*   By: SarahLouise <SarahLouise@student.42.fr>      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/31 18:53:57 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/04/14 15:31:24 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/04/22 19:31:22 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+// git testing
 
 int	ft_arrlen(char **arr)
 {
